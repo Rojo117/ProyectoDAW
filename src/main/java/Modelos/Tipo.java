a28afe4f94ad5d1a31d8bd61e1d34ef103f9b17e
conej <conejo_pumba@hotmail.com>
2023-06-08 18:45:04 -0500

    Tipo Java Bean

---
+++ src/main/java/Modelos/Tipo.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelos;
+
+/**
+ *
+ * @author conej
+ */
+public class Tipo {
+
+    int idTipo;
+    String descripcion;
+
+    public Tipo() {
+    }
+
+    public int getIdTipo() {
+        return idTipo;
+    }
+
+    public void setIdTipo(int idTipo) {
+        this.idTipo = idTipo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+}
