055a13504056facc8e3798a955dfd35e590ed3c5
conej <conejo_pumba@hotmail.com>
2023-06-08 18:39:01 -0500

    colecciones Java Bean

---
+++ src/main/java/Modelos/colecciones.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelos;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author conej
+ */
+public class colecciones implements Serializable {
+    int id_coleccion;
+    String descripcion;
+    public colecciones() {}
+
+    public int getId_coleccion() {
+        return id_coleccion;
+    }
+
+    public void setId_coleccion(int id_coleccion) {
+        this.id_coleccion = id_coleccion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+}
