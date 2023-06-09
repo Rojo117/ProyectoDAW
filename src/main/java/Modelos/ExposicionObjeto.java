267fc96831233bb42a0efd1bacc761b1417eb04c
conej <conejo_pumba@hotmail.com>
2023-06-08 19:09:56 -0500

    ExposicionObjeto jb

---
+++ src/main/java/Modelos/ExposicionObjeto.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelos;
+
+/**
+ *
+ * @author conej
+ */
+public class ExposicionObjeto {
+int idExposicion;
+	int idObraDeArte;
+    public ExposicionObjeto() {
+    }
+
+    public int getIdExposicion() {
+        return idExposicion;
+    }
+
+    public void setIdExposicion(int idExposicion) {
+        this.idExposicion = idExposicion;
+    }
+
+    public int getIdObraDeArte() {
+        return idObraDeArte;
+    }
+
+    public void setIdObraDeArte(int idObraDeArte) {
+        this.idObraDeArte = idObraDeArte;
+    }
+    
+}
