34d4278f014b163649a1f5c6297b4bc0a5162568
conej <conejo_pumba@hotmail.com>
2023-06-08 19:14:20 -0500

    escutura JB

---
+++ src/main/java/Modelos/escultura.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelos;
+
+/**
+ *
+ * @author conej
+ */
+public class escultura {
+    int idObraDeArte;
+    String material;
+    int altura;
+    int peso;
+    String estilo;
+    public escultura() {
+    }
+
+    public int getIdObraDeArte() {
+        return idObraDeArte;
+    }
+
+    public void setIdObraDeArte(int idObraDeArte) {
+        this.idObraDeArte = idObraDeArte;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public String getEstilo() {
+        return estilo;
+    }
+
+    public void setEstilo(String estilo) {
+        this.estilo = estilo;
+    }
+    
+}
