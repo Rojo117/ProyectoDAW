eb0c44305663ea91f907b60ceb3948bf4f7a9b5a
conej <conejo_pumba@hotmail.com>
2023-06-08 19:17:16 -0500

    otros JB

---
+++ src/main/java/Modelos/otros.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelos;
+
+/**
+ *
+ * @author conej
+ */
+public class otros {
+int idObraDeArte;
+  String  tipo;
+    String estilo;
+    public otros() {
+    }
+
+    public int getIdObraDeArte() {
+        return idObraDeArte;
+    }
+
+    public void setIdObraDeArte(int idObraDeArte) {
+        this.idObraDeArte = idObraDeArte;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getEstilo() {
+        return estilo;
+    }
+
+    public void setEstilo(String estilo) {
+        this.estilo = estilo;
+    }
+    
+}
