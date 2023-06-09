de1359705547793d4b79aa676ef9100cb863b953
conej <conejo_pumba@hotmail.com>
2023-06-08 19:15:54 -0500

    estatuas JB

---
+++ src/main/java/Modelos/estatuas.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelos;
+
+/**
+ *
+ * @author conej
+ */
+public class estatuas {
+int idObraDeArte;
+    int altura;
+    int peso;
+  String  representacion;
+    public estatuas() {
+    }
+
+    public int getIdObraDeArte() {
+        return idObraDeArte;
+    }
+
+    public void setIdObraDeArte(int idObraDeArte) {
+        this.idObraDeArte = idObraDeArte;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public String getRepresentacion() {
+        return representacion;
+    }
+
+    public void setRepresentacion(String representacion) {
+        this.representacion = representacion;
+    }
+    
+}
