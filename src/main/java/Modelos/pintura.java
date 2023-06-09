1a7749af5ac289fffacbac2d356250c3b1988619
conej <conejo_pumba@hotmail.com>
2023-06-08 19:12:15 -0500

    pintura JB

---
+++ src/main/java/Modelos/pintura.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelos;
+
+/**
+ *
+ * @author conej
+ */
+public class pintura {
+    int idObraDeArte;
+    String tipo_pintura;
+    String material;
+    String estilo;
+    public pintura() { }
+
+    public int getIdObraDeArte() {
+        return idObraDeArte;
+    }
+
+    public void setIdObraDeArte(int idObraDeArte) {
+        this.idObraDeArte = idObraDeArte;
+    }
+
+    public String getTipo_pintura() {
+        return tipo_pintura;
+    }
+
+    public void setTipo_pintura(String tipo_pintura) {
+        this.tipo_pintura = tipo_pintura;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public String getEstilo() {
+        return estilo;
+    }
+
+    public void setEstilo(String estilo) {
+        this.estilo = estilo;
+    }
+    
+}
