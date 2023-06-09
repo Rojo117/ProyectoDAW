59f5463a16efe3e6fc33de4c5e6b5e3a5aafa33c
conej <conejo_pumba@hotmail.com>
2023-06-08 19:00:09 -0500

    Artistas Java Bean

---
+++ src/main/java/Modelos/artistas.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelos;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author conej
+ */
+public class artistas {
+ String nombre;
+   Date fecha_nacimiento;
+   Date fecha_fallecimiento;
+   String pais_origen;
+    int epoca;
+    String estilo;
+    String descripcion;
+    public artistas() {
+    }
+    
+}
